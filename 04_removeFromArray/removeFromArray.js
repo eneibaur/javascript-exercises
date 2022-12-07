dd1d8ee5d7e59d39176c5d988cdeb2f0e3f31362
eneibaur <107266929+eneibaur@users.noreply.github.com>
2022-12-07 05:53:11 -0800

    Completed excercise four

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let filter = [...args];
+    let answer = array;
+    for (let i = 0; i < array.length; i++){
+        for (let j = 0; j < filter.length; j++){
+            if (answer[i] === filter[j]){
+                answer.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return answer;
 };
+
+// look at each item in the array
+// compare it to each item in the arguments array
+// if it does match, remove the value from answer.
 
 // Do not edit below this line
 module.exports = removeFromArray;
