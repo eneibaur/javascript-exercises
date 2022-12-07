7b0f5a4688ab1fd46d86114e25f8f5f8085a113f
eneibaur <107266929+eneibaur@users.noreply.github.com>
2022-12-07 06:41:27 -0800

    Complete excercise seven

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let cel = ((temp - 32) * (5/9));
+  let answer = Math.round(cel * 10) / 10;
+  return answer;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let answer = Math.round(((temp * (9/5)) + 32) * 10) / 10;
+  return answer;
 };
 
 // Do not edit below this line
