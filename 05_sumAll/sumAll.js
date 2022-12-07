84bd9f3077a0bd583564d0b5bf1e20c8b4fb0af9
eneibaur <107266929+eneibaur@users.noreply.github.com>
2022-12-07 06:12:53 -0800

    Complete excercise five

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a !== "number" || typeof b !== "number" ||
+    a < 0 || b < 0)
+        return 'ERROR';
+    let highNum = b;
+    let lowNum = a;
+    if (a > b) {
+        highNum = a;
+        lowNum = b;
+    }
+    let answer = 0;
+    for (let i = lowNum; i <= highNum; i++) {
+        answer += i;
+    }
+    return answer;
 };
 
 // Do not edit below this line
