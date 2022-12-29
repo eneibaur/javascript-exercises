1825e15f90fd93a9bb47e6692b811fd045d87cc8
eneibaur <107266929+eneibaur@users.noreply.github.com>
2022-12-29 12:27:12 -0800

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+    let num = parseInt(nth);
+    if (num <= 0){
+        return "OOPS";
+    };
+    let fib = [1, 1];
+    for (let i = 2; i < num; i++){
+        let fibNext = fib[i-1] + fib[i-2];
+        fib.push(fibNext);
+    };
+    return answer = fib[nth - 1];
 };
 
 // Do not edit below this line
