eb09d482e50318a19a6b5d41b2c985382d2d6541
eneibaur <107266929+eneibaur@users.noreply.github.com>
2022-12-29 09:00:01 -0800

    completed palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    const arr = Array.from(words).filter(l => l.match(/[a-z]/i))
+    let arrCopy = [...arr];
+    let reversedArr = arr.reverse();
+    arrCopy = arrCopy.join("").toLowerCase();
+    reversedArr = reversedArr.join("").toLowerCase();
+    return reversedArr == arrCopy ? true : false;
 };
 
 // Do not edit below this line
