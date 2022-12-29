05a7cb37278dfd3620b5130896bcc142af982632
eneibaur <107266929+eneibaur@users.noreply.github.com>
2022-12-29 12:36:54 -0800

    completed getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = books.map(book => book.title);
+    console.log(titles);
+    return titles;
 };
 
 // Do not edit below this line
